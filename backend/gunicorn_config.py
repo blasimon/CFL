3bcc3b43f4c7f562497e292eb054254ce1a5e606
alex-pythonista <indrajit.alex@gmail.com>
2024-02-05 01:17:05 +0600

    gunicorn_config.py changed

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,7 +1,8 @@
 import multiprocessing
 
 # Calculate the number of workers (adjust the multiplier based on your application and server)
-workers = multiprocessing.cpu_count() * 2 + 1
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 
 bind = "0.0.0.0:8000"
 timeout = 120
